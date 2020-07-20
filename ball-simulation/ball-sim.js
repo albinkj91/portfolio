89f056c0e358c230db12aec9981fad435c250b73
albinkj91 <albinkj91@yahoo.com>
2020-07-20 23:34:22 +0200

    Changed rendering algorithm

--- ball-simulation/ball-sim.js
+++ ball-simulation/ball-sim.js
@@ -54,18 +54,18 @@ class Ball{
 		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
 		ctx.stroke();
 
-		ctx.fillText(frameCount, 50, 50);
+		ctx.fillText("FPS: " + frameCount, 50, 50);
 	}
 
 	move(){
 		let gravityVector = new Vector(0, this.gravity);
 		this.vector.add(gravityVector);
 
-		this.x += this.vector.x * 0.01;
-		this.y += this.vector.y * 0.01;
+		this.x += this.vector.x * 0.001;
+		this.y += this.vector.y * 0.001;
 
 		if(this.y > 714 && Math.abs(this.vector.x) > 0){
-			this.vector.x -= this.vector.x * 0.01;
+			this.vector.x -= this.vector.x * 0.005;
 			console.log(this.vector.x);
 		}
 		console.log(this.x);
@@ -105,27 +105,29 @@ class Ball{
 	}
 }
 
-function render(){	
-	requestAnimationFrame(render);
-
-	now = Date.now();
-	elapsed = now - then;
-
-	if(elapsed > fpsInterval){
-		frameCount = Math.floor(1000 / (now - then));
-		then = now - (elapsed % fpsInterval);
-	}
-	
+function update(){
 	ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 	balls.forEach(ball => ball.move());
 	balls.forEach(ball => ball.checkBounds());
 	balls.forEach(ball => ball.draw(ctx));
 }
 
+function render(){	
+	requestAnimationFrame(render);
+
+	now = performance.now();
+	elapsed = now - then;
+
+	if(elapsed > fpsInterval){
+		frameCount = Math.floor(1000 / (now - then));
+		then = now - (elapsed % fpsInterval);
+	}
+}
+
 let frameCount = 0;
 let fps = 60;
 let fpsInterval = 1000 / fps;
-let then = Date.now();
+let then = performance.now();
 let startTime = then;
 let now;
 let elapsed;
@@ -140,12 +142,18 @@ let balls = [];
 let heldBall;
 let mouseStartX;
 let mouseStartY;
+let mouseDown = false;
 
 canvas.addEventListener("mousedown", event => {
+	if(mouseDown){
+		return;
+	}
+
 	heldBall = new Ball(event.offsetX, event.offsetY, 50, new Vector(0, 0), 0, "#fca311");
 	balls.push(heldBall);
 	mouseStartX = event.offsetX;
 	mouseStartY = event.offsetY;
+	mouseDown = true;
 });
 
 canvas.addEventListener("mousemove", event => {
@@ -156,10 +164,16 @@ canvas.addEventListener("mousemove", event => {
 });
 
 canvas.addEventListener("mouseup", event => {
-	heldBall.vector.x = (event.offsetX - mouseStartX) * 10;
-	heldBall.vector.y = (event.offsetY - mouseStartY) * 10;
+	if(!mouseDown){
+		return;
+	}
+
+	heldBall.vector.x = (event.offsetX - mouseStartX) * 30;
+	heldBall.vector.y = (event.offsetY - mouseStartY) * 30;
 	heldBall.gravity = 9.82 * 5;
 	heldBall = null;
+	mouseDown = false;
 });
 
 requestAnimationFrame(render);
+setInterval(update, 1);
